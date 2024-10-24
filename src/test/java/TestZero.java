aa3322cc7916223ee78bd032ed6d7fda52eb58d1
alkaz07 <alkaz07@yandex.ru>
2024-10-24 21:01:56 +0300

    Initial commit

---
+++ src/test/java/TestZero.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class TestZero {
+    @Test
+    public void zero(){
+        Assertions.assertEquals(4, 2*2);
+    }
+}
